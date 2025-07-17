1b7bae68a57aa9bb6f582e9adc43768a5dd73a5d
Karan Kumar Sah <krotrn.ks@gmail.com>
2025-07-17 18:51:55 +0530

    Updated nested driver folder

---
+++ Driver/src/app/bookings/page.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import BookingsPage from '@/components/BookingsPage';
+
+export default function DriverBookingsPage() {
+  return <BookingsPage />;
+} 
---
+++ Driver/src/app/bus/page.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import BusPage from '@/components/BusPage';
+
+export default function DriverBusPage() {
+  return <BusPage />;
+} 
